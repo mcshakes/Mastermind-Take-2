d8116058ba62bfb9f4716806fc2c7c14bee41eef
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-02 16:49:41 -0700

    need more tests. Especially for randomness...

--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/mastermind'
+# require '../lib/mastermind' ##This allows me to run individual tests.
+# above with one dot allows me to run rake. why?
 
 class MastermindTest < Minitest::Test
   def test_it_exists
@@ -9,6 +11,7 @@ class MastermindTest < Minitest::Test
   # add
 
   def test_it_wins
+    skip
     mm = Mastermind.new
     result = mm.execute("BBGB")
     assert result.downcase.include?("win")
