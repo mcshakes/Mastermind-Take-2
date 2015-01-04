09bfa504f5b79e4f38ef8328e1957e41e05f5b46
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-04 14:47:45 -0700

    Are these tests pedantic? Probably.

--- test/guess_validator_test.rb
+++ test/guess_validator_test.rb
@@ -5,22 +5,39 @@ require './lib/guess_validator'
 class GuessValidatorTest < Minitest::Test
 
   def test_it_exists
-    validator = GuessValidator.new(guess, colors='rgby')
+    # validator = GuessValidator.new(guess, colors='rgby')
     assert GuessValidator.new
   end
 
   def test_guess_is_too_long
-    skip
     validator = GuessValidator.new
+    secret_code = "RBGY"
     guess = "BRRBGY"
-    assert validator.too_long?(guess)
+    assert validator.too_long?(guess, secret_code)
     guess = "RBGY"
-    refute validator.too_long?(guess)
+    refute validator.too_long?(guess, secret_code)
   end
 
-  def test_guess_includes_valid_characters_only
-    skip
-
+  def test_guess_is_too_short
+    validator = GuessValidator.new
+    secret_code = "RBGY"
+    guess = "BR"
+    assert validator.too_short?(guess, secret_code)
+    guess = "RBGY"
+    refute validator.too_short?(guess, secret_code)
   end
 
+  def test_guess_includes_invalid_characters
+    validator = GuessValidator.new
+    guess = %w(T 9 Q Z)
+    possible_colors = %w(R G B Y)
+    assert validator.invalid?(guess, possible_colors)
+  end
+
+  def test_guess_is_all_valid
+    validator = GuessValidator.new
+    guess = %w(R G B Y)
+    possible_colors = %w(R G B Y)
+    assert validator.invalid?(guess, possible_colors)
+  end
 end
