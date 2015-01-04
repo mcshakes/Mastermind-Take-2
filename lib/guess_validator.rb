69736b569ba34666ecf329b2a05f7a8d0526d809
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-04 14:48:52 -0700

    happy with invalid? method I guess. See comments for reminder

--- lib/guess_validator.rb
+++ lib/guess_validator.rb
@@ -1,19 +1,34 @@
 # this will check for color matches against user_input
 require_relative 'codemaker'
 
-
 class GuessValidator
-  # attr_reader :guess, :possible_colors
-  #
-  # def initialize(guess, secret_code)
-  #   @guess = guess
-  #   @secret_code = secret_code
-  # end
-  #
-  # def too_long?(guess)
-  #   if guess.length == secret_code.length
-  #     false
-  #   end
+  attr_reader :guess, :possible_colors
+
+  # def initialize(guess, colors='rgby')
+  #   self.guess = guess
+  #   self.possible_colors = colors
   # end
 
+  def possible_colors
+    %w(R G B Y)
+  end
+
+  def too_long?(guess, secret_code)
+    guess.length > secret_code.length
+  end
+
+  def too_short?(guess, secret_code)
+    guess.length < secret_code.length
+  end
+
+  # This is only checking for true/false in invalidity?
+  # What if you want to ask which are true/false?
+
+  def invalid?(guess, possible_colors)
+    guess.find {|color| /[^{possible_colors}]+/ =~ color}
+    # .find only gets one letter that is incorrect. Is that enough?
+  end
 end
+
+val = GuessValidator.new
+val.invalid?(%w(T 9 Q Z), %w(R G B Y))
