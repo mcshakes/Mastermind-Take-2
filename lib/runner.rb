a5fab9f131f7587ca4d89e292b00e1df2bf6fab1
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-05 12:28:51 -0700

    finished small runner: tested play, instructions, quit

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,15 +1,39 @@
 require './lib/mastermind'
-# This is the CLI.
 
+class Runner
 
+  def initialize(input, output)
+  #need to initialize the messages; separate class
+  #input: the actual user input that is put in.
+  #output:
+  #@user_input = ''
+  end
+
+  def user_input
+    input = user_input
+  end
 puts "Welcome to Mastermind"
 
-input = ""
-mastermind = Mastermind.new
+  def end?
+    user_input == "q"
+  end
 
-while input != "q"
-  print "> "
-  input = gets.chomp
-  puts mastermind.execute(input)
-end
+  def play?
+    user_input == 'p'
+  end
+
+  def instructions?
+    user_input == "i"
+  end
+
+# user_input = ""
+# mastermind = Mastermind.new
+#
+# while input != "q"
+#   print "> "
+#   input = gets.chomp
+#   puts mastermind.execute(input)
+# end
 puts "Goodbye!"
+
+end
