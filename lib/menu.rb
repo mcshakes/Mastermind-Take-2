bd923a0d834f7ad6dd27ec36f23b26c64042c076
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-06 18:43:23 -0700

    assimilating method names between classes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,12 @@
+require './lib/printer'
+require 'pry'
+
 class Menu
+  attr_reader :printer
+
+  def initialize
+    @printer = Printer.new
+  end
 
   def active?
     @active
@@ -6,7 +14,7 @@ class Menu
 
   def start
     @active = true
-    return ["let's play a game. q or p. choose.", :go] # flesh out in printer.rb
+    return [printer.welcome, :go] # displays Welcome message
   end
 
   def execute(input)
@@ -15,44 +23,8 @@ class Menu
     when 'i' then ["instructions go here", :continue]
     when 'p'
       @active = false
-      ["starting game", :start_game]
+      [@printer.game_start, :start_game]
     end
   end
-
-  # def player_choices
-  #   output.puts printer.welcome
-  #   until end?
-  #     @user_input = input.gets.strip.downcase
-  #     menu_sequence
-  #   end
-  #   output.puts printer.end_game
-  # end
-  #
-  # def menu_sequence
-  #   case
-  #   when play?
-  #     Mastermind.new.play   # play is execute or play the game
-  #   when instructions?
-  #     output.puts printer.instructions
-  #   when end?
-  #     output.puts printer.end_game
-  #     abort
-  #   else
-  #     output.puts printer.invalid_command
-  #   end
-  # end
-  #
-  # def end?
-  #   user_input == "q"
-  # end
-  #
-  # def play?
-  #   user_input == 'p'
-  # end
-  #
-  # def instructions?
-  #   user_input == "i"
-  # end
-
 
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,5 +1,6 @@
 require './lib/mastermind'
 require './lib/printer'
+require 'pry'
 
 class Runner
   attr_reader :printer, :menu, :game
@@ -11,18 +12,10 @@ class Runner
   end
 
   def run
-    # this is only thing in Runner.rb
-    # while we should keep going...
-      # get input from user
-      # if menu is active, send it to the menu
-      # if the game is active, send input to the game
-      # print feedback from menu/game
-      # figure out if we should keep going
     feedback = menu.start
     message = feedback[0]
     signal = feedback[1]
-
-    puts message
+    puts message            # Is this the welcome message?
 
     until signal == :stop || signal == :broken
     # until [:stop, :broken].include?(signal)
@@ -46,10 +39,8 @@ class Runner
       else
         feedback = ["uhh... what's happening?", :broken]
       end
-
       message = feedback[0]
       signal = feedback[1]
-
       puts message
     end
   end
