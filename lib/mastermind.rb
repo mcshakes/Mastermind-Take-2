5c3433b65d45bd1702d2d9482fbf0474d77f2936
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-06 18:41:02 -0700

    starting to connect the two and passing tests

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,18 +1,59 @@
-class Mastermind
+require './lib/codemaker'
+require './lib/guess_evaluator'
+require './lib/guess_validator'
+require './lib/printer'
+require './lib/runner'
+require './lib/menu'
 
+class Mastermind
+  attr_reader :sequence, :printer, :codemaker
+
+  def initialize
+    @codemaker = Codemaker.new
+    @messages = Printer.new
+  end
+
+  def start
+    @active = true
+    @sequence = Codemaker.new
+    return ["I've generated....", :go]
+  end
 
   def execute(input)
-    # if game is in progress
-    # keep taking guesses through input.
-    # input is the same input from runner.rb std.in
+    # signal == :start_game
+     ["game_start", :play]
+     # prompt user for the choice
+     # Take choice, compare to
+     # .lib/guess_eval and guess_validator
+    ## Until correct, print out
+     # messages showing correct colors and positions
+    ## If correct, go back to signal :win from runner.rb
 
-    # secret = "BBGB"
-    # if input == secret
-    #   "You win!"
+    # if guess is right, queue :win method
+    ["Winner message!", :win] # remember the win message and the
     # else
-    #   "Guess again!"
-    # end
+    #   give feedback
+    # flesh out gameplay for ["2 correct colors, 1 correct position", :continue]
+
+
+  end
+
+  def active?
+    @active
+  end
+
+  def generate_sequence
+    # ["R", "Y", "B", "G"].shuffle
+    @codemaker.secret_code
+  end
+  # binding.pry
+
+  def stop
+  # Where the you quit from runner.rb
+  #
+  end
+
+  def win
+    # Then return to main menu
   end
 end
-
-#This is game.rb
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,21 +1,43 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/mastermind'
-# require '../lib/mastermind' ##This allows me to run individual tests.
+
+# require '../lib/mastermind' ##This only allows me to run individual tests.
 # above with one dot allows me to run rake. why?
 
 class MastermindTest < Minitest::Test
 
   def test_it_exists
-    assert MastermindTest
+    assert Mastermind.new
   end
 
   def test_it_starts_the_game
-
+    mm = Mastermind.new
+    menu = Menu.new
+    printer = Printer.new
+    feedback = menu.execute('p')
+    message = feedback[0]
+    signal = feedback[1]
+    assert_equal :start_game, signal
+    assert_equal printer.game_start, message
   end
 
-  def test_it_enters_gameflow_after_p_is_selected
+  def test_it_generates_sequences
+    mm_1 = Mastermind.new
+    mm_1.start
+    mm_2 = Mastermind.new
+    mm_2.start
+    assert mm_1.sequence != mm_2.sequence
+  end
 
+  def test_the_randomness_of_sequences
+    mm = Mastermind.new
+    sequences = []
+    100.times do
+      sequences << mm.generate_sequence
+    end
+    unique_sequences = sequences.uniq
+    assert unique_sequences.count > 1
   end
 
   def test_it_wins_if_correct
@@ -28,4 +50,21 @@ class MastermindTest < Minitest::Test
   def test_it_brings_back_to_main_menu_after_win
     skip
   end
+
+  def test_it_wins_first_try
+    skip
+    mm = Mastermind.new
+    mm.start
+    answer = mm.sequence  # remove this hardcoded in .start
+    feedback = mm.execute(answer)
+    message = feedback[0]
+    signal = feedback[1]
+    assert_equal :win, signal
+  end
+
+  def test_it_brings_back_to_main_menu_after_win
+    skip
+  end
+
+
 end
